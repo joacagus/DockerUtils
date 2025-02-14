c84b840e01045bd5905bf06746e43046d6ec8986
joacagus <agusjoalopez@gmail.com>
2025-02-14 12:27:20 +0100

    7.0: Added comments to code

--- gitdocker/app/app.py
+++ gitdocker/app/app.py
@@ -1,7 +1,8 @@
+'''Importamos flask a nuestra instancia de python'''
 from flask import Flask
 
 app = Flask(__name__)
-
+'''El test para ver si funciona'''
 @app.route('/')
 def hello():
     return "Hello Habib, its for you "
