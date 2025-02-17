3f2015465e45820f7237003f3cbb01dde2b04817
joacagus <agusjoalopez@gmail.com>
2025-02-17 12:41:48 +0100

    For testing if redis works

--- gitdocker/app/app.py
+++ gitdocker/app/app.py
@@ -9,3 +9,27 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+
+    '''import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f'Hello World! I have been seen {count} times.\n' '''
